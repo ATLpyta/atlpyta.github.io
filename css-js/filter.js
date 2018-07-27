c46f5ffa1d0c0dcf21014bbbcb7ad6514ce43ef3
Adel Ferdjoukh <ferdjoukh@gmail.com>
2018-07-27 13:55:07 +0200

    script for filtering MTs

--- css-js/filter.js
+++ css-js/filter.js
@@ -16,26 +16,28 @@ function filterMTs(){
 	var rulesMax=rulesSliderInputs[1].value;
 
 	var helpersSliderInputs= document.getElementById("helpers-slider").getElementsByTagName('input');
-	var helpersMin=helpersSliderInputs[0].value;
-	var helpersMax=helpersSliderInputs[1].value;
+	var helpersMin= parseInt(helpersSliderInputs[0].value);
+	var helpersMax= parseInt(helpersSliderInputs[1].value);
 
 	//Show only the MTs that are in the right intervals
 	//
 	var MTcontainer=document.getElementById("MT-container");
 
-	for (var i = 0; i<= MTcontainer.getElementsByClassName("row MT").length - 25; i++) {
-			
-		nbMTs++;
-		MTcontainer.getElementsByClassName("row MT")[i].style.display = "none";
-
-
-		//MTcontainer.getElementsByClassName("row MT")[i].style.display = "block";
+	for (var i = 0; i<= MTcontainer.getElementsByClassName("row MT").length - 1; i++) {
+		
+		var criteriaList= MTcontainer.getElementsByClassName("row MT")[i].getElementsByTagName("li");
+		var nbRules= parseInt(criteriaList[0].innerHTML.substring(7));
+		var nbHelpers= parseInt(criteriaList[3].innerHTML.substring(9));
+		
+		if(rulesMin <= nbRules && nbRules <= rulesMax && helpersMin <= nbHelpers && nbHelpers <= helpersMax ){
+			nbMTs++;
+		    MTcontainer.getElementsByClassName("row MT")[i].style.display = "block";
+		}else{
+			MTcontainer.getElementsByClassName("row MT")[i].style.display = "none";
+		}
 	}
-
 
 	//Update the number of filtered MTs
 	//
-	document.getElementById("nbMT").innerHTML = MTs-nbMTs;	
-	
-	alert("Rules ["+rulesMin+","+rulesMax+"]"+" Helpers ["+helpersMin+","+helpersMax+"]"+nbMTs+"/"+MTs);
+	document.getElementById("nbMT").innerHTML = nbMTs;	
 }
