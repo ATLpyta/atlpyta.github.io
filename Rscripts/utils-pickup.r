f4e39dd0e21a7cd343b70fff87da5b8b78572bd7
Adel Ferdjoukh <ferdjoukh@gmail.com>
2018-08-07 14:45:14 +0200

    R script for creating strategies files

--- Rscripts/utils-pickup.r
+++ Rscripts/utils-pickup.r
@@ -16,8 +16,20 @@
 #	modelTransformations: a vector containing the names of result ModelTransformation
 
 createStrategyFile <- function(filePath,schemafilePath,description,modelTransformations){
-	
+	absolutefilePath=paste0("../selection-strategies/",filePath)
+	file.create(absolutefilePath)
+
+	openedFile<-file(absolutefilePath)
+	writeLines(c(description,schemafilePath,modelTransformations),openedFile)
+	close(openedFile)
 }
 
 
 #Unit tests
+modelTransformations<- vector()
+modelTransformations<- c(modelTransformations, "A2B")
+modelTransformations<- c(modelTransformations, "ATLCopier")
+modelTransformations<- c(modelTransformations, "ATLCopier")
+createStrategyFile("mono-testStrategy","avg-schema.png",
+	"Model transformation containing the minimum number of Rules (between 0 and Decile 2) are selected"
+	,modelTransformations)
