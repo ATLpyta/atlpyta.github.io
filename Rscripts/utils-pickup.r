cf8299ab94e9c820a0bc573b74dbafeeb4db94c6
Adel Ferdjoukh <ferdjoukh@gmail.com>
2018-08-07 14:21:41 +0200

    utils R functions

---
+++ Rscripts/utils-pickup.r
@@ -0,0 +1,23 @@
+#This function creates the strategies files
+# 
+# Each file contains the following line:
+#
+#	description line
+#	description schema filePath
+#	result Model Transformation 1
+#   Model Transformation 2
+#   ...
+#	Model Transformation n
+#
+# PARAMETERS
+#	filePath: relative file path contained in folder [selection-strategies/]
+#	schemafilePath: relative file path contained in folder [selection-strategies/img/]
+#	description: paragraph describing the strategy HTML
+#	modelTransformations: a vector containing the names of result ModelTransformation
+
+createStrategyFile <- function(filePath,schemafilePath,description,modelTransformations){
+	
+}
+
+
+#Unit tests
